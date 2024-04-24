92ab628b672684b46560988a0153fcc0e50eeeab
olex <olexandr.samus.2004@gmail.com>
2024-04-24 19:19:30 +0300

    added hw1

---
+++ hw1/test.py
@@ -0,0 +1,13 @@
+# створити дві змінні, задати їм значення з консолі, і зробити між ними операції
+# + - * / // % **
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** 2)
+print(b ** 2)
